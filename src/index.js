b9e6d1c424cd0d91912ebb73b69ad9366b80684e
MimiMagusin <mimi_magusin@hotmail.com>
2018-05-23 12:07:09 +0200

    Set up Queries

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { render } from "react-dom";
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
@@ -6,40 +6,94 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 const client = new ApolloClient({
-  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+  uri: "https://nx9zvp49q7.lp.gql.zone/graphql"
 });
 
-const ExchangeRates = () => (
-  <Query
-    query={gql`
-      {
-        rates(currency: "USD") {
-          currency
-          rate
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
+const GET_DOGS = gql`
+  {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
 
-      return data.rates.map(({ currency, rate }) => (
-        <div key={currency}>
-          <p>{`${currency}: ${rate}`}</p>
+const GET_DOG_PHOTO = gql`
+  query dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+
+const Dogs = ({ onDogSelected }) => (
+  <Query query={GET_DOGS}>
+    {({ loading, error, data }) => {
+      if (loading) return "Loading...";
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <div>
+          <select name="dog" onChange={onDogSelected}>
+            {data.dogs.map(dog => (
+              <option key={dog.id} value={dog.breed}>
+                {dog.breed}
+              </option>
+            ))}
+          </select>
         </div>
-      ));
+      );
     }}
   </Query>
 );
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      <ExchangeRates />
-    </div>
-  </ApolloProvider>
+const DogPhoto = ({ breed }) => (
+  <Query
+    query={GET_DOG_PHOTO}
+    variables={{ breed }}
+    skip={!breed}
+    pollInterval={5000}
+    notifyOnNetworkStatusChange
+  >
+    {({ loading, error, data, refetch, networkStatus }) => {
+      if (networkStatus === 4) return "Refetching!";
+      if (loading) return null;
+      if (error) return `Error!: ${error}`;
+
+      return (
+        <div>
+          <img
+            src={data.dog.displayImage}
+            style={{ height: 100, width: 100 }}
+          />
+          <button onClick={() => refetch()}>Refetch!</button>
+        </div>
+      );
+    }}
+  </Query>
 );
+
+class App extends Component {
+  state = { breed: 'affenpinscher'}
+
+  onDogSelected = ({ target }) => {
+    this.setState(() => ({ breed: target.value }));
+  };
+
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>
+          <h2>My first Apollo app 🚀</h2>
+          <Dogs onDogSelected={this.onDogSelected}/>
+          <DogPhoto breed={this.state.breed} />
+        </div>
+      </ApolloProvider>
+    )
+  }
+ 
+};
 
 render(<App />, document.getElementById("root"));
